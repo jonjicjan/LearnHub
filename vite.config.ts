c52ea9b3e004c7ed76bc1447e61596cc4f4a3895
jonjicjan <codebykhan99@gmail.com>
2025-01-12 13:51:10 +0530

    ApI Testing 0.1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/LearnHub",
+  base:"/LearnHub/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
